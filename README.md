828635e2a001fb04f4627f56548f532686199eb5
floofysaur <aricia.chan06@gmail.com>
2024-08-05 18:05:09 -0400

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Todo
+
+- build outline
+- style it
+- init git repo
+- add more social links
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="hero">
+        <h1>Hi I'm Aricia</h1>
+        <p>Designer. Illustrator. Cat hugger.</p>
+      </div>
+
+      <div className="projects">
+        <div className="card">
+          <h2>RBC</h2>
+        </div>
+        <div className="card">
+          <h2>Scoreswipe</h2>
+        </div>
+        <div className="card">
+          <h2>Blackjack counter</h2>
+        </div>
+        <div className="card">
+          <h2>Illustration</h2> 
+        </div>
+      </div>
+      <div className="footer">
+        <p>find me on LinkedIn!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
